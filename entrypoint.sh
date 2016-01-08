93c75a66483c391f00e88f82724b383664270206
Per Abich <per.abich@gmail.com>
2016-01-08 07:17:45 +0000

    Fixed variable in entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-/consul/consul agent -server -bootstrap-expect=1 -config-file consul-config.json -ui-dir=/consul/dist -client=0.0.0.0 @*
+/consul/consul agent -server -config-file consul-config.json -ui-dir=/consul/dist -client=0.0.0.0 $@
